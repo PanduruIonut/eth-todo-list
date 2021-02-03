303e179128ba8095d675d40586afc2d19340b088
PanduruIonut <panduru.ionut@hotmail.com>
2021-02-03 22:18:47 +0200

    load contracts & account,fix truffle contracts import to work, add rendering states

--- src/app.js
+++ src/app.js
@@ -1,13 +1,74 @@
+import("../vendor/@truffle/contract/dist/truffle-contract.js");
+App = {
+    loading: false,
+    contracts: {},
 
-     ethEnabled = () => {  if (window.ethereum) {
-          window.web3 = new Web3(window.ethereum);
-              window.ethereum.enable();
-                  return true;
-                  }
-                   return false;}
+    load: async () => {
+        await App.loadWeb3()
+        await App.loadAccount()
+        await App.loadContract()
+        await App.render()
+    },
 
+    loadWeb3: () => {  
+        
+        if (typeof web3 !== 'undefined') {
+            App.web3Provider = web3.currentProvider
+            web3 = new Web3(web3.currentProvider)
+          } else {
+            window.alert("Please connect to Metamask.")
+        }
+        
+        if (window.ethereum) {
+            window.web3 = new Web3(window.ethereum);
+            window.ethereum.enable();
+            return true;
+        }
+        return false;
+    },
+
+    loadAccount: async ()=> {
+        App.account = await web3.eth.getAccounts();
+    },
+
+    loadContract: async () => {
+        // Create a Js version of the smart contract
+        const todoList = await $.getJSON('TodoList.json')
+        App.contracts.TodoList = TruffleContract(todoList)
+        App.contracts.TodoList.setProvider(App.web3Provider)
+        
+        // Hydrate the smart contract with value from blockchain
+        App.todoList = await App.contracts.TodoList.deployed()
+    },
+
+    render: async () => {
+        if(App.loading) {
+            return
+        }
+
+        App.setLoading(true)
+
+        $('#account').html(App.account)
+
+        App.setLoading(false)
+    },
+    
+    setLoading: (boolean) => {
+        App.loading = boolean
+        const loader = $('#loader')
+        const content = $('#content')
+        if (boolean) {
+          loader.show()
+          content.hide()
+        } else {
+          loader.hide()
+          content.show()
+        }
+      }
+    }
+                  
     $(()=> {
         $(window).load(() => {
-            ethEnabled()
+            App.load()
         })
     })
